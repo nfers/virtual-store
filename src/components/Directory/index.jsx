7a25d93fe00ba67d009bc7ccfbd749fa53385287
Nayara Ferreira <ncy.fers@gmail.com>
2020-06-14 12:50:12 -0300

    implementado estilizacao para as imagens se moverem conforme a posicao do mouse

--- src/components/Directory/index.jsx
+++ src/components/Directory/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import "./directory.scss";
-import MenuItem from './../Menu-Item/Index';
+import MenuItem from "./../Menu-Item/Index";
 
 /*component statefull*/
 class Directory extends React.Component {
@@ -13,46 +13,44 @@ class Directory extends React.Component {
         {
           title: "chapeus",
           imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-          size: 'large',
+          size: "large",
           id: 1,
         },
         {
           title: "jaquetas",
           imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-          size: 'large',
+          size: "large",
           id: 2,
         },
         {
           title: "tenis",
           imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-          size: 'large',
-          id: 2,
+          size: "large",
+          id: 3,
         },
         {
           title: "mulheres",
           imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-          size: 'large',
+          size: "large",
           id: 4,
         },
         {
           title: "homens",
           imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-          size: 'large',
+          size: "large",
           id: 5,
-        }]
-    }
+        },
+      ],
+    };
   }
   render() {
     return (
       <div className="directory-menu">
-        {
-          this.state.sections.map(({ title, imageUrl, id, size }) => (
-            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
-          ))
-        }
+        {this.state.sections.map(({ title, imageUrl, id, size }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        ))}
       </div>
-    )
+    );
   }
-
 }
 export default Directory;
