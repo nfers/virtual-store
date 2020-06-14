9d504ca599e41081d04c0792f534e60c220d2fbc
Nayara Ferreira <ncy.fers@gmail.com>
2020-06-14 14:46:12 -0300

    adicionando página Inicial do projeto

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './homepage.scss'
+import Directory from './../../components/Directory/index';
+
+const HomePage = ()  => (
+    <div className="homepage">
+      <Directory />
+    </div>
+)
+
+export default HomePage;
