bd8d0668cd5729ae0a10a94883c5d0c908d13749
Nayara Ferreira <ncy.fers@gmail.com>
2020-06-14 12:10:57 -0300

    ajuste de tamanho na estilização da imagem

--- src/components/Directory/index.jsx
+++ src/components/Directory/index.jsx
@@ -13,26 +13,31 @@ class Directory extends React.Component {
         {
           title: "chapeus",
           imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+          size: 'large',
           id: 1,
         },
         {
           title: "jaquetas",
           imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+          size: 'large',
           id: 2,
         },
         {
           title: "tenis",
           imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+          size: 'large',
           id: 2,
         },
         {
           title: "mulheres",
           imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+          size: 'large',
           id: 4,
         },
         {
           title: "homens",
           imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+          size: 'large',
           id: 5,
         }]
     }
@@ -41,8 +46,8 @@ class Directory extends React.Component {
     return (
       <div className="directory-menu">
         {
-          this.state.sections.map(({title, imageUrl, id}) => (
-            <MenuItem key={id} title={title} imageUrl={imageUrl}/>
+          this.state.sections.map(({ title, imageUrl, id, size }) => (
+            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
           ))
         }
       </div>
